0c2cd9361ab08f1dd396f8553eed0ca38f342bb2
ShaziaNasir <148135973+ShaziaNasir@users.noreply.github.com>
2024-03-23 23:49:12 +0500

    simple calculator added

---
+++ index.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+
+// asking questions from user through inquirer
+
+let answers=await inquirer.prompt([
+    {message:"Enter First Number",type:"number",name:"firstNumber"},
+    {message:"Enter Second Number",type:"number",name:"secondNumber"},
+{
+    message:"Select one Operator to perform Operations",
+    type:"list",
+    name:"operator",
+    choices:["Addition","Subtraction","Multiplication","Division"]
+},
+
+]);
+// conditional statements if/else
+
+if(answers.operator=== "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if(answers.operator ==="Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+else if(answers.operator ==="Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if(answers.operator ==="Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log("invalid input")
+}
+
